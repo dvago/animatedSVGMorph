bffab2675e92c01985dac86f3b3b692ddac09a8f
Davide Vago <pigroz@gmail.com>
2023-08-20 08:36:49 +0100

    Clean up the file _u_animatedsvg.js

--- _u_animatedsvg.js
+++ _u_animatedsvg.js
@@ -1,73 +1,68 @@
-import Snap from 'snapsvg';
+import Snap from "snapsvg";
 
 class animatedSVG {
-
-    constructor(options) {
-
-        if(!options.el) {
-            return false; // you need to specify an el entry
-        }
-
-        this.el = options.el;
-
-        this.defaults = {
-                initialPath: options.steps[0].animation.d,
-                viewBox: '0 0 0 0',
-                steps: [],
-                beforeAnimationStart: () => {},
-                onAnimationEnd: () => {}
-        };
-
-        this.options = Object.assign(this.defaults, options);
-
-        this.initialize();
+  constructor({
+    el,
+    steps = [0],
+    attrs,
+    initialPath,
+    viewBox = "0 0 0 0",
+    easing,
+    beforeAnimationStart = () => {},
+    onAnimationEnd = () => {}
+  } = {}) {
+    if (!el) {
+      console.error("You need to specify a DOM element as entry.");
+      return false;
     }
 
-    initialize() {
-        this.createSnap();
-        this.attachEvents();
+    this.el = el;
+    this.options = {
+      initialPath: initialPath || steps[0].animation.d,
+      viewBox,
+      steps,
+      attrs,
+      easing,
+      beforeAnimationStart,
+      onAnimationEnd
+    };
+
+    this.initialize();
+  }
+
+  initialize() {
+    this.createSnap();
+    this.attachEvents();
+  }
+
+  createSnap() {
+    this.snap = Snap(this.el);
+    this.snap.attr(this.options.attrs);
+
+    this.path = this.snap.path(this.options.initialPath);
+  }
+
+  animation(el, steps, easing, frame, callback) {
+    if (frame >= steps.length) {
+      callback && callback();
+      return false;
     }
 
-    createSnap() {
-        this.snap = Snap(`#${this.el}`);
-        this.snap.attr(this.options.attrs);
+    el.animate(steps[frame].animation, steps[frame].dur, easing, () => {
+      this.animation(el, steps, easing, frame + 1, callback);
+    });
+  }
 
-        this.path = this.snap.path(this.options.initialPath);
-    }
-
-    animation(el, steps, easing, frame, callback) {
-
-        if(frame >= steps.length) {
-
-            if(callback) {
-                callback();
-            }
-
-            return false;
-        }
-
-        el.animate(
-            steps[frame].animation,
-            steps[frame].dur,
-            easing,
-            () => { this.animation(el, steps, easing, frame + 1, callback); }
-        );
-
-    }
-
-    attachEvents() {
-
-        Promise.resolve()
-            .then( () => {
-            this.options.beforeAnimationStart();
-            })
-            .then( () => {
-                    this.animation(this.path, this.options.steps, this.options.easing, 1, this.options.onAnimationEnd);
-            });
-
-    }
-
+  attachEvents() {
+    this.options.beforeAnimationStart();
+    this.animation(
+      this.path,
+      this.options.steps,
+      this.options.easing,
+      1,
+      this.options.onAnimationEnd
+    );
+  }
 }
-
 
 export default animatedSVG;
